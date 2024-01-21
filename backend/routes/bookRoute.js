6f7eae6efdd3747fcab1bc016d48713bc01989a6
Mahamed Ahmed <mahamedahmed@Mahameds-MacBook-Air.local>
2024-01-21 21:17:51 +0300

    added express router for all the endpoints and seperated it from the main code

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -5,10 +5,37 @@ import { Book } from '../models/bookModel.js'
 
 const router = express.Router()
 
-//routes for creating a new book
+
+//route for getting all the books
+router.get('/', async (req, res) => {
+    console.log("Are you there?")
+    try {
+        const books = await Book.find({});
+        console.log(books)
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        res.status(400).send({ message: error.message })
+
+    }
+})
+// //route for getting a books
+router.get('/:id', async (req, res) => {
+
+    try {
+        const books = await Book.findById(req.params.id);
+        return res.status(200).json(books)
+    } catch (error) {
+        res.status(400).send({ message: error.message })
+
+    }
+})
+// //routes for creating a new book
 router.post('/', async (req, res) => {
     const { title, author, year } = req.body
-       const old = await  Book.up.findOne({title: req.body.title})
+    const old = await Book.findOne({ title: req.body.title })
     try {
         if (old != null || !req.body.title || !req.body.author || !req.body.year) {
             return res.status(201).send({ message: "Please send the correct book info" })
@@ -30,37 +57,7 @@ router.post('/', async (req, res) => {
 
 })
 
-//route for getting a books
-router.get('/:id', async (req, res) => {
-
-    try {
-        const books = await Book.findById(req.params.id);
-        return res.status(200).json(books)
-    } catch (error) {
-        res.status(400).send({ message: error.message })
-
-    }
-
-})
-
-//route for getting all the books
-// router.get('/', async (req, res) => {
-//     console.log("Are you there?")
-//     try {
-//         const books = await Book.find({});
-//         console.log(books)
-//         return res.status(200).json({
-//             count: books.length,
-//             data: books
-//         })
-//     } catch (error) {
-//         res.status(400).send({ message: error.message })
-
-//     }
-
-// })
-
-//route for Deleting all the books
+// //route for Deleting all the books
 router.delete('/', async (req, res) => {
     try {
         const books = await Book.deleteMany({})
@@ -70,19 +67,35 @@ router.delete('/', async (req, res) => {
     }
 })
 
-//route for Deleting a book
+// //route for Deleting a book
 router.delete('/:id', async (req, res) => {
     try {
         const book = await Book.findByIdAndDelete(req.params.id);
-        if(!book){
-            return res.status(404).json({message: 'Book not found!'})
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found!' })
         }
-        return res.status(200).send({message: 'Sucessfully deleted!'})
+        return res.status(200).send({ message: 'Sucessfully deleted!' })
     } catch (error) {
         res.status(400).send({ message: error.message })
 
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.year) {
+            return res.status(201).send({ message: "Please send the correct book info" })
+        }
+        const book = await Book.findByIdAndUpdate(req.params.id,
+            { title: req.body.title, author: req.body.author, year: req.body.year })
+        if (!book) {
+            return res.status(201).send({ message: "Did not updated" })
+        }
+        return res.status(200).send({ message: "Updated successfully!" })
+
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+})
 
 export default router;
