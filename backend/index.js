ca2567faf5be9636925df4a619f91c368e257900
Mahamed Ahmed <mahamedahmed@Mahameds-MacBook-Air.local>
2024-01-24 11:57:04 +0300

    added tables and cards for showing the books

--- backend/index.js
+++ backend/index.js
@@ -3,96 +3,22 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import router from './routes/bookRoute.js';
+import cors from 'cors'
 const app = express();
-//const router = express.Router()
 
+//option 1: allows all origin with default of Cors(*)
+// app.use(cors())
+
+//option 2: Allws custom Origins and only clients with these origins can access the server
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
 //middleware for parsing request body
 app.use(express.json())
 app.use('/books', router);
 
-// app.get('/', (req, res)=>{
-
-//     return res.status(123).send("Welcome to MERN Stack Project!")
-
-// })
-//route for getting all the books
-// router.get('/', async (req, res) => {
-//     console.log("Are you there?")
-//     try {
-//         const books = await Book.find({});
-//         console.log(books)
-//         return res.status(200).json({
-//             count: books.length,
-//             data: books
-//         })
-//     } catch (error) {
-//         res.status(400).send({ message: error.message })
-
-//     }
-
-// })
-
-// //routes for creating a new book
-// app.post('/', async (req, res) => {
-//     const { title, author, year } = req.body
-//        const old = await  Book.up.findOne({title: req.body.title})
-//     try {
-//         if (old != null || !req.body.title || !req.body.author || !req.body.year) {
-//             return res.status(201).send({ message: "Please send the correct book info" })
-//         }
-//         const newBook = {
-//             title: req.body.title,
-//             author: req.body.author,
-//             year: req.body.year
-//         }
-//         Book.create(newBook).
-//             then((book) => {
-//                 return res.status(200).send(book)
-
-//             })
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).send({ message: error.message })
-//     }
-
-// })
-
-// //route for getting a books
-// app.get('/:id', async (req, res) => {
-
-//     try {
-//         const books = await Book.findById(req.params.id);
-//         return res.status(200).json(books)
-//     } catch (error) {
-//         res.status(400).send({ message: error.message })
-
-//     }
-
-// })
-
-// //route for Deleting all the books
-// app.delete('/', async (req, res) => {
-//     try {
-//         const books = await Book.deleteMany({})
-//         res.status(200).json(books)
-//     } catch (error) {
-//         res.status(400).send({ message: error.message })
-//     }
-// })
-
-// //route for Deleting a book
-// app.delete('/:id', async (req, res) => {
-//     try {
-//         const book = await Book.findByIdAndDelete(req.params.id);
-//         if(!book){
-//             return res.status(404).json({message: 'Book not found!'})
-//         }
-//         return res.status(200).send({message: 'Sucessfully deleted!'})
-//     } catch (error) {
-//         res.status(400).send({ message: error.message })
-
-//     }
-// })
 
 mongoose.connect(mongoDBURL)
     .then(() => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import ShowBook from './pages/ShowBook'
-import EditBook from './pages/EditBook'
-import CreateBooks from './pages/CreateBooks'
-import Home from './pages/Home'
-import DeleteBook from './pages/DeleteBook'
-import {Routes, Route} from 'react-router-dom'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
-
-export default function App() {
+const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/books' element={<EditBook/>}/>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
+  );
+};
 
-  )
-}
+export default App;
